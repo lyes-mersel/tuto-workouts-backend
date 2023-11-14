2d7a31731a3bc2f34d10a572b9f209bd0fc8ab8d
lyes-mersel <dev.mersel@gmail.com>
2023-11-14 22:58:52 +0100

    Updating createWorkoutController: return the empty fiels in the error message

--- controllers/workoutController.js
+++ controllers/workoutController.js
@@ -31,6 +31,22 @@ getOneWorkout = async (req, res) => {
 // create a new workout
 createWorkout = async (req, res) => {
 	const { title, reps, load } = req.body;
+	let emptyFields = [];
+
+	if (!title) {
+		emptyFields.push("title");
+	}
+	if (!reps) {
+		emptyFields.push("reps");
+	}
+	if (!load) {
+		emptyFields.push("load");
+	}
+	if (emptyFields.length > 0) {
+		return res
+			.status(400)
+			.json({ error: "Please fill in all the fields !", emptyFields });
+	}
 
 	await Workout.create({ title, reps, load })
 		.then((workout) => res.status(200).json(workout))
