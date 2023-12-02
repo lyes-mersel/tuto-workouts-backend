257c077530c566d2d43d513d0d9c10ecedd5305d
lyes-mersel <dev.mersel@gmail.com>
2023-12-02 16:33:05 +0100

    Backend: complete the singup & login configurations

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,34 @@
 const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 
+// create a Json Web Token
+function createToken(_id) {
+	return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
+}
+
 // user login
-function loginUser(req, res) {
-	res.json({ mssg: "User login" });
+async function loginUser(req, res) {
+	const { email, password } = req.body;
+	try {
+		const user = await User.login(email, password);
+		const token = createToken(user._id);
+		res.status(200).json({ email, token });
+	} catch (error) {
+		res.status(400).json({ mssg: error.message });
+	}
 }
 
 // user signup
-function signupUser(req, res) {
-	res.json({ mssg: "User signup" });
+async function signupUser(req, res) {
+	const { email, password } = req.body;
+	try {
+		const user = await User.signup(email, password);
+		const token = createToken(user._id);
+		res.status(200).json({ email, token });
+	} catch (error) {
+		res.status(400).json({ mssg: error.message });
+	}
 }
 
 module.exports = { loginUser, signupUser };
--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema({
 	email: {
@@ -11,5 +13,56 @@ const userSchema = new mongoose.Schema({
 		required: true,
 	},
 });
+
+// signup static method
+userSchema.statics.signup = async function (email, password) {
+	// validations
+	if (!email || !password) {
+		throw Error("All fiels must be filled");
+	}
+	if (!validator.isEmail(email)) {
+		throw Error("Email is not valid");
+	}
+	if (!validator.isStrongPassword(password)) {
+		throw Error("Password is not strong enough");
+	}
+
+	// check if the email is already in use
+	const userExists = await this.findOne({ email: email });
+	if (userExists) {
+		throw Error("Email already in use !");
+	}
+
+	// hashing
+	const salt = await bcrypt.genSalt(10);
+	const hash = await bcrypt.hash(password, salt);
+	const user = await this.create({ email: email, password: hash });
+
+	return user;
+};
+
+// login static method
+userSchema.statics.login = async function (email, password) {
+	// validation
+	if (!email || !password) {
+		throw Error("All fiels must be filled");
+	}
+
+	// check if the user exists
+	const user = await this.findOne({ email: email });
+	if (!user) {
+		// incorrect email address
+		throw Error("Incorrect login credentials !");
+	}
+
+	// compare the password with the hash
+	const match = await bcrypt.compare(password, user.password);
+	if (!match) {
+		// incorrect password
+		throw Error("Incorrect login credentials !");
+	}
+
+	return user;
+};
 
 module.exports = mongoose.model("User", userSchema);
